7f3eb7b6b25da4da8e73855222f7cbd93060a5f4
Umer Farooq <farooq.umer1602@gmail.com>
2026-01-20 22:49:46 +0500

    Update Footer component with new design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,7 +26,7 @@ const socialIcons = [
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-200 py-10">
+    <footer className="bg-gray-900 mt-3 text-gray-200 py-10">
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-8">
 
         {/* Brand */}
